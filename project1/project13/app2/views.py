bab3d97e3419e3976cbdd6f5cfe0ba2fd87a7c6a
Anjali2071207120 <chinthaanjali681@gmail.com>
2023-01-07 21:40:10 +0530

    first commit

---
+++ project1/project13/app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def a2_one(request):
+    return render(request,'a2_one.html')
+
---
+++ project19/app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Barbie(request):
+    return render(request,'Barbie')
+def flower(request):
+    return render(request,'flower')
+def lordkrishna(request):
+    return render(request,'lordkrishna')
+
